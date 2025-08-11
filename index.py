968fbd6ee31ba790a6bb366fd82f778c3714fdb9
tanishqGarg12 <122337335+tanishqGarg12@users.noreply.github.com>
2025-08-11 20:44:23 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,39 +1,3 @@
-# print("Script started")
-# import trino
-# from trino.auth import BasicAuthentication
-# import csv
-
-# conn = trino.dbapi.connect(
-#     host='qaas-eu.svc.nlsn.media',
-#     port=443,
-#     user='tanishq.garg',
-#     catalog='hive-mdl',
-#     schema='digital_ads_ref_prod',
-#     auth=BasicAuthentication("tanishq.garg", "95be8f22a8cf4f38a84fc3a50b1c92a4")
-# )
-# print(conn)
-
-# try:
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT 1")
-#     cursor.fetchall()  # Fetch to ensure the connection is valid
-#     print("Authentication and connection successful.")
-#     cursor.execute("SELECT * FROM ref_campaign_prepared LIMIT 10")
-#     rows = cursor.fetchall()
-#     print("First 10 rows from ref_campaign_prepared:")
-#     for row in rows:
-#         print(row)
-
-#     # Export to CSV
-#     with open("output.csv", "w", newline="") as f:
-#         writer = csv.writer(f)
-#         writer.writerows(rows)
-#     print("Data exported to output.csv")
-
-# except Exception as e:
-#     print("Authentication failed:", e)
-#     exit(1)
-
 
 print("Script started")
 import trino
@@ -41,12 +5,12 @@ from trino.auth import BasicAuthentication
 import pandas as pd
 
 conn = trino.dbapi.connect(
-    host='qaas-eu.svc.nlsn.media',
-    port=443,
-    user='tanishq.garg',
-    catalog='hive-mdl',
-    schema='digital_ads_ref_prod',
-    auth=BasicAuthentication("tanishq.garg", "95be8f22a8cf4f38a84fc3a50b1c92a4")
+    host='',
+    port=,
+    user='',
+    catalog='',
+    schema='',
+    auth=BasicAuthentication("", "")
 )
 print(conn)
 
